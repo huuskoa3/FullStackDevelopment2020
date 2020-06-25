dfe73f9d868cde230e282f58bc22f1befd1c0d1d
huuskoa3 <38131104+huuskoa3@users.noreply.github.com>
2020-06-25 15:56:23 +0300

    Add files via upload

---
+++ osa1/Unicafe/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <p>Good: {props.good}</p>
+      <p>Neutral: {props.neutral}</p>
+      <p>Bad: {props.bad}</p>
+      <p>All: {props.good-props.bad}</p>
+      <p>Average: {(props.good+props.neutral+props.bad)/3}</p>
+      <p>Positive: {(props.good/((props.good+props.neutral+props.bad)))*100} %</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick = {()=>setGood(good+1)}> good </button>
+      <button onClick = {()=>setNeutral(neutral+1)}> neutral </button>
+      <button onClick = {()=>setBad(bad+1)}> bad </button>
+      <h1>Statistics</h1>
+      <Statistics good = {good} neutral = {neutral} bad = {bad}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
