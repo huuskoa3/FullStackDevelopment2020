a30eb0da196fffab264860298bcced951d561784
Aarni <aarni.huuskonen@aalto.fi>
2020-07-02 14:44:52 +0300

    2.2

--- osa2/tests/src/App.js
+++ osa2/tests/src/App.js
@@ -8,12 +8,12 @@ const App = () => {
     id: 1,
     parts: [
       {
-        name:'Fundamentals of React',
+        name: 'Fundamentals of React',
         exercises: 10,
         id: 1
       },
       {
-        name:'Using props to pass data',
+        name: 'Using props to pass data',
         exercises: 7,
         id: 2
       },
@@ -21,6 +21,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Rejex',
+        exercises: 11,
+        id: 4
       }
     ]
   }
--- osa2/tests/src/components/Course.js
+++ osa2/tests/src/components/Course.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom'
 
 
 const Course = ({course}) => {
+  let sum = 0
+  course.parts.map((a) =>  sum = sum + a.exercises)
+
   return (
     <div>
     <h1>{course.name}</h1>
     {course.parts.map(tag =>
-    <p key = {tag.id}>{tag.name} {tag.exercises}</p>) }
+      <p key = {tag.id}>
+      {tag.name} {tag.exercises}</p>)}
+    <b>total of {sum} exercises</b>
     </div>
   )
 }
