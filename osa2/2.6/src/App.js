3f5bbce8dab3c94961a6545c0d98bb8f7670aebb
Aarni <aarni.huuskonen@aalto.fi>
2020-07-04 00:43:06 +0300

    lisää

---
+++ osa2/2.6/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+
+// metodi luo uuden olion personObject ja antaa lisää tämän persons listaan
+// jonka lisäksi metodi palauttaa lisättävän henkilön nimen tyhjäksi
+// merkkijonoksi
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+// tapahtumakäsittelijä päivittää uuden lisäyksen setNewName tilaa
+// näyttämällä tekstin syötteessä (input)
+  const handlePersonAddition = (event) => {
+      setNewName(event.target.value)
+  }
+
+
+// App käsittelee uuden henkilön lisäyksen addPerson ja handlePersonAddition
+// metodien avulla. Tulostus tapahtuu map komennolla
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+          name: <input
+          value={newName}
+          onChange={handlePersonAddition}
+          />
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map(person =>
+        <p key = {person.name}> {person.name}</p>)}
+    </div>
+  )
+
+}
+
+export default App
