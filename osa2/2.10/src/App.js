864f5c20905d8bb101116480a1d3b89349108d08
Aarni <aarni.huuskonen@aalto.fi>
2020-07-06 01:26:36 +0300

    lisäys

---
+++ osa2/2.10/src/App.js
@@ -0,0 +1,129 @@
+import React, { useState } from 'react'
+
+
+// komponentti palauttaa syötteen joka suodattaa nimiä
+const Filter = (props) => {
+  return (
+    <div>
+      filter shown with: <input
+      onChange={props.changeFunc}
+      value={props.value}
+      />
+    </div>
+  )
+}
+
+// komponentti palauttaa uuden nimen ja numeron lisäävät syötteet (input)
+// ja lähetyspainikkeen (button)
+const PersonForm = (props) => {
+  return (
+    <div>
+    <form onSubmit={props.submit}>
+      <div>
+        name: <input
+        value={props.valName}
+        onChange={props.changeName}
+        />
+      </div>
+      <div>
+        number: <input
+        value={props.valNumber}
+        onChange={props.changeNumber}
+        />
+      </div>
+    <div>
+        <button type="submit">add</button>
+    </div>
+    </form>
+    </div>
+  )
+}
+
+// komponentti palauttaa näytettävien henkilöiden nimet ja numerot
+const Persons = (props) => {
+  return (
+    <div>
+      {props.showFunction.map(person =>
+        <p key = {person.name}> {person.name} {person.number}</p>)}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
+  const [ showAll, setShowAll ] = useState(true)
+
+// metodi luo uuden olion personObject ja antaa lisää tämän persons listaan
+// jonka lisäksi metodi palauttaa lisättävän henkilön nimen JA NUMERON tyhjiksi
+// merkkijonoiksi
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    if(persons.filter(person => person.name === newName).length > 0) {
+      return window.alert(`${newName} is already added to phonebook`)
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+// metodi määrittää showAll tilan, ja näin näyttää henkilöt joiden nimessä
+// esiintyy merkkijono newFilter
+  const namesToShow = showAll
+      ? persons
+      : persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+
+// tapahtumakäsittelijä päivittää uuden lisäyksen setNewName tilaa
+// näyttämällä tekstin syötteessä (input)
+  const handlePersonAddition = (event) => {
+      setNewName(event.target.value)
+  }
+
+// tapahtumakäsittelijä päivittää uuden lisäyksen setNewNumber tilaa
+// näyttämällä tekstin syötteessä (input)
+  const handlePersonNumberAddition = (event) => {
+      setNewNumber(event.target.value)
+  }
+
+// tapahtumakäsittelijä päivittää uuden lisäyksen setNewFilter tilaa
+// näyttämällä tekstin syötteessä (input)
+  const handleFilterAddition = (event) => {
+    setNewFilter(event.target.value)
+    console.log(persons.filter(person => person.name.includes(newFilter) !== true))
+    if(newFilter !== '') {
+      setShowAll(persons.filter(person => person.name.includes(newFilter) !== true) > 0)
+    } else {
+      setShowAll(true)
+    }
+  }
+
+// App käsittelee uuden henkilön lisäyksen addPerson ja handlePersonAddition
+// metodien avulla. Tulostus tapahtuu map komennolla
+// 2.10 toteutettu komponentit Filter, PersonForm ja Persons
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter changeFunc = {handleFilterAddition} value = {newFilter}/>
+      <h2>Add a new</h2>
+      <PersonForm submit = {addPerson}
+        valName = {newName} changeName = {handlePersonAddition}
+        valNumber = {newNumber} changeNumber = {handlePersonNumberAddition}
+      />
+      <h2>Numbers</h2>
+      <Persons showFunction = {namesToShow}/>
+    </div>
+  )
+}
+
+export default App
