9195a223e1e715b4dbb891d2a4125a05ff59b72a
“Aarni” <aarni.huuskonen95@gmail.com>
2020-07-08 19:50:11 +0300

    lisäys

--- osa2/2.12/src/App.js
+++ osa2/2.12/src/App.js
@@ -3,36 +3,63 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 
-const promise = axios.get('https://restcountries.eu/rest/v2/all')
-console.log(promise)
-
-const Filter = (props) => {
-  console.log("filter run")
+//komponentti päivittää syötetekstin ja aktivoi tapahtumakäsittelijän
+//handleFilterAddition kun syöte muuttuu
+const Filter = ({changeFunc, value}) => {
   return (
     <div>
       find countries: <input
-      onChange={props.changeFunc}
-      value={props.value}
+      onChange={changeFunc}
+      value={value}
       />
     </div>
   )
 }
 
+// komponentti palauttaa maiden listan tai yhden maan tiedot
 const Render = (props) => {
-  console.log("render run")
-  return (
-    <div>
-      {props.showFunction.map(country =>
-        <p key = {country.name}> {country.name} </p>)}
-    </div>
-  )
+  const x = props.show
+  if(x.length > 10 ){
+    return (
+      <p> Too many matches </p>
+    )
+  }
+  else if(x.length <= 10 && x.length > 1) {
+    return (
+      <div>
+        {x.map(country =>
+          <p key = {country.name}>{country.name}</p>)}
+      </div>
+    )
+  }
+  else if(x.length === 1) {
+    return (
+      <div>
+        {x.map(country =>
+          <h1 key = {country.name}> {country.name}</h1>)}
+        {x.map(country =>
+          <p key = {country.name}> capital: {country.capital}</p>)}
+        {x.map(country =>
+          <p key = {country.name}> population: {country.population}</p>)}
+        <h1> Languages </h1>
+        {x.map(country =>
+          <ul key = {country.name}> {country.languages.map(l => <li key = {l.name}> {l.name} </li>)}</ul>)}
+        {x.map(country =>
+          <img key = {country.name} src = {country.flag} width ="150px" alt = "img"></img>)}
+      </div>
+    )
+  }else{
+    return(
+      <p> No matches </p>
+    )
+  }
 }
 
 
 const App = () => {
 
   const [countries, setCountries] = useState([])
-  const [showAll, setShowAll] = useState(true)
+  const [showAll, setShowAll] = useState(false)
   const [newFilter, setNewFilter] = useState('')
 
   useEffect(() => {
@@ -43,30 +70,21 @@ const App = () => {
       })
   }, [])
 
+
   const countriesToShow = showAll
       ? countries
       : countries.filter(country => country.name.toLowerCase().includes(newFilter.toLowerCase()))
 
   const handleFilterAddition = (event) => {
-    console.log(newFilter)
     setNewFilter(event.target.value)
-  //  console.log(countries.filter(country => country.name.includes(newFilter) !== true))
-      if(newFilter !== '') {
-        setShowAll(countries.filter(country => country.name.includes(newFilter) !== true) > 0)
-      } else {
-        setShowAll(true)
-      }
-
   }
 
   return (
     <div>
       <Filter changeFunc = {handleFilterAddition} value = {newFilter}/>
-      <p> {console.log("GAP")} </p>
-      <Render showFunction = {countriesToShow}/>
+      <Render show = {countriesToShow}/>
     </div>
   )
-
 }
 
 export default App;
